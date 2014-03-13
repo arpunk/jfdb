5b57b92885dd39fdcdfca1e42d74c74d7e22bff7
arpunk <arpunk@cryptolab.net>
2014-03-12 19:51:58 -0500

    andamiaje de funciones de migración

--- src/Teatros/Migrate.hs
+++ src/Teatros/Migrate.hs
@@ -1,2 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Teatros.Migrate where
+
+import qualified Data.Text as T
+import qualified Data.ByteString as BS
+
+-- | Leer CSV
+leerArchivo :: String -> IO BS.ByteString
+leerArchivo = BS.readFile . (++) "data/"
+
+-- | Construcción de tipos de documentos
+migrarPeriodico :: IO ()
+migrarPeriodico = undefined
+
+migrarProgramaMano :: IO ()
+migrarProgramaMano = undefined
+
+migrarAfiche :: IO ()
+migrarAfiche = undefined
+
+migrarFotografia :: IO ()
+migrarFotografia = undefined
+
+migrarAudiovisual :: IO ()
+migrarAudiovisual = undefined
+
+migrarBibliografia :: IO ()
+migrarBibliografia = undefined
+
+migrarPremio :: IO ()
+migrarPremio = undefined
+
+migrarObraGrafica :: IO ()
+migrarObraGrafica = undefined
+
+migrarActividadCultural :: IO ()
+migrarActividadCultural = undefined
