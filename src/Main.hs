b7ff99ee80d3c4e88a9a11fd0f4e3bc5a098abec
arpunk <arpunk@cryptolab.net>
2014-04-24 01:34:53 -0500

    agrego main y elimino instrucciones de benchmarks (no hay)

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,17 @@
 module Main where
 
 import Teatros.Migrate
-import Teatros.Default
-import Teatros.Persistent
-import Teatros.Parse
+import Teatros.Types
 
 main :: IO ()
-main = return ()
+main = do
+  decodificarCsv "periodicos.txt"             EntidadPeriodico
+  decodificarCsv "programas-mano.txt"         EntidadProgramaMano
+  decodificarCsv "afiches.txt"                EntidadAfiche
+  decodificarCsv "fotografias.txt"            EntidadFotografia
+  decodificarCsv "audiovisuales.txt"          EntidadAudiovisual
+  decodificarCsv "bibliografias.txt"          EntidadBibliografia
+  decodificarCsv "premios.txt"                EntidadPremio
+  decodificarCsv "obras-graficas.txt"         EntidadObraGrafica
+  decodificarCsv "actividades-culturales.txt" EntidadActividadCultural
+  decodificarCsv "programas-academicos.txt"   EntidadProgramaAcademico
